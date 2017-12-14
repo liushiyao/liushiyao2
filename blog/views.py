8ca1fcf88772889e95d64c955f124e161d6976b0
liushiyao <walrkson@hotmail.com>
2017-12-14 17:51:22 +0800

    blog雏形搭建

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
+#coding=utf-8
 from django.shortcuts import render
+from blog.models import BlogsPost
 
 # Create your views here.
+def index(request):
+    blog_list = BlogsPost.objects.all()
+    return render(request, 'index.html', {'blog_list':blog_list})
